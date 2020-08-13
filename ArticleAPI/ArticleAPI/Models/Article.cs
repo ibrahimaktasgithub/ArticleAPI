aeb9ebf36fefe9c358848feede5318e7045eaecb
ibrahim <ibrahimaktas93@gmail.com>
2020-08-13 15:25:55 +0300

    Database oluşturulması ve proje içerisine eklenmesi

---
+++ ArticleAPI/ArticleAPI/Models/Article.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArticleAPI.Models
+{
+    public partial class Article
+    {
+        public Article()
+        {
+            Post = new HashSet<Post>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string Text { get; set; }
+        public string Slug { get; set; }
+
+        public ICollection<Post> Post { get; set; }
+    }
+}
---
+++ ArticleAPI/ArticleAPI/Models/ArticleDBContext.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ArticleAPI.Models
+{
+    public partial class ArticleDBContext : DbContext
+    {
+        public ArticleDBContext()
+        {
+        }
+
+        public ArticleDBContext(DbContextOptions<ArticleDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Article> Article { get; set; }
+        public virtual DbSet<Post> Post { get; set; }
+
+        //        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //        {
+        //            if (!optionsBuilder.IsConfigured)
+        //            {
+        ////#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+        //                optionsBuilder.UseSqlServer("Server=DESKTOP-38A5O5H;Database=ArticleDB;UID=devExp;PWD=159753q;");
+        //            }
+        //        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Article>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Author)
+                    .HasColumnName("AUTHOR")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .HasColumnName("NAME")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Slug)
+                    .HasColumnName("SLUG")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Text)
+                    .HasColumnName("TEXT")
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Post>(entity =>
+            {
+                entity.Property(e => e.PostId).HasColumnName("POST_ID");
+
+                entity.Property(e => e.CategoryId).HasColumnName("CATEGORY_ID");
+
+                entity.Property(e => e.CreatedDate)
+                    .HasColumnName("CREATED_DATE")
+                    .HasColumnType("datetime");
+
+                entity.Property(e => e.Description)
+                    .HasColumnName("DESCRIPTION")
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Title)
+                    .HasColumnName("TITLE")
+                    .HasMaxLength(2000)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.Category)
+                    .WithMany(p => p.Post)
+                    .HasForeignKey(d => d.CategoryId)
+                    .HasConstraintName("FK__Post__CATEGORY_I__398D8EEE");
+            });
+        }
+    }
+}
